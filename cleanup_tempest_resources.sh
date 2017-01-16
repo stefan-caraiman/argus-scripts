7467eea2c1aec23fac53c86e428b45ab8968931c
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2017-01-16 17:06:57 +0200

    Update the cleanup script

--- cleanup_tempest_resources.sh
+++ cleanup_tempest_resources.sh
@@ -2,18 +2,17 @@
 
 source /home/cb-init/keystone_rc
 
-# Delete all leftover routers created by tempest
-neutron router-list | awk '{if ($2 && $2 != "ID" && $4 ~ /^ *tempest/) {print $2}}' | xargs -n1 neutron router-delete
+# Delete any unused floating ips
+neutron floatingip-list | awk '$2 && $2 != "ID" {print $2}' | xargs -n1  neutron floatingip-delete
 
 # Delete all the networks
 neutron net-list | awk '{if ($2 && $2 != "ID" && $4 ~ /^ *tempest/) {print $2}}' | xargs -n1 neutron net-delete
 
-# Delete any unused floating ips
-neutron floatingip-list | awk '$2 && $2 != "ID" {print $2}' | xargs -n1  neutron floatingip-delete
+# Delete all leftover routers created by tempest
+neutron router-list | awk '{if ($2 && $2 != "ID" && $4 ~ /^ *tempest/) {print $2}}' | xargs -n1 neutron router-delete
 
 # Delete projects
 openstack project list | awk '{if ($2 && $2 != "ID" && $4 ~ /^ *tempest/) {print $2}}' | xargs -n1 openstack project delete
 
 # Delete users
 openstack user list | awk '{if ($2 && $2 != "ID" && $4 ~ /^ *tempest/) {print $2}}' | xargs -n1 openstack user delete
-
